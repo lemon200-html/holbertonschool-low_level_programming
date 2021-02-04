020f8a527dcff3beafb1477707118ce3c72b7ad4
lemon200-html <lemon200@gmail.com>
2021-02-04 16:33:58 +0000

    c project

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main-third_program
+ *Return:Ends_the-program
+ */
+
+int main(void)
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
