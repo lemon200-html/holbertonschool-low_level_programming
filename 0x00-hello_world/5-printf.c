f326dbca38ca083ee7641f807a1e7eabd00fb3ba
lemon200-html <lemon200@gmail.com>
2021-02-04 14:45:37 +0000

    c project

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main-second_program
+ *Return:Ends_the_program
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
