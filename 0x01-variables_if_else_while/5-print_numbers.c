c60f1e2e415bf5c0a432fc36643c8530cd50a15c
lemon200-html <lemon200@gmail.com>
2021-02-05 21:25:43 +0000

    c project

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main-number
+ *Return:Ends the program
+ */
+
+int main(void)
+{
+	char ch = '0';
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+		putchar('\n');
+
+	return (0);
+}
