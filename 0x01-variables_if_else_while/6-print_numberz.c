039e440303b8cfaf6a175b2fec2da59aaeffa136
lemon200-html <lemon200@gmail.com>
2021-02-05 21:33:50 +0000

    c project

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main-Numberz
+ *Return:Ends the program
+ */
+
+int main(void)
+{
+	int ch = '0';
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+		putchar('\n');
+
+	return (0);
+}
