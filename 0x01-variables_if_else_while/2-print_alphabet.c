4cd173e93519fb2c63a3f5bf522d8371d7cf8920
lemon200-html <lemon200@gmail.com>
2021-02-05 20:34:00 +0000

    c project

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main-alphabet
+ *Return:Ends the program
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+		putchar('\n');
+
+	return (0);
+}
